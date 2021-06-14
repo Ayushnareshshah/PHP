36e92eadf0d90158cc3b65ba2ab83265fd85da18
ayush shah <42650409+Ayushnareshshah@users.noreply.github.com>
2021-06-14 16:10:12 +0530

    Form.php
    Form.php

---
+++ Day 4/FROM.php
@@ -0,0 +1,20 @@
+<html>
+    <body>
+        <form>
+            <table>
+                <tr>
+                    <td>Name</td>
+                    <td><input type="text" name="txt1" /></td>
+                </tr>
+                <tr>
+                    <td>Age</td>
+                    <td><input type="number" name="txt2" /></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" />Submit</td>
+                    <td><input type="reset"/></td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
